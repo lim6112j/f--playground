08f8e13e6c2247bdd8136102540f10e5d3e12510
lim6112j <lim6112j@gmail.com>
2022-06-26 02:30:41 +0900

    inittial commit

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+let prefix prefixStr baseStr = prefixStr + " , " + baseStr
+let names = [ "David"; "Maria"; "Alex" ]
+let prefixPartial = prefix "Hello"
+let exclaim s = s + "!"
+
+names
+|> Seq.map prefixPartial
+|> Seq.map exclaim
+|> Seq.sort
